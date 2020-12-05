41e99a852ba07b30d7d13f8a4d55ef0b3445d78c
greenmac <alwaysmac@msn.com>
2020-12-05 11:23:21 +0800

    Day009_基礎語言模型N_Gram實作作業

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-class Some:
-    def __init__(self, x):
-        self.x = x
-    
-    def service(self, y):
-        print('do service...', self.x + y)
+set1 = set()# 建立空的集合
 
-class Other:
-    pass
+set2 = {1, 2, 3, 4, 5}
 
-o = Other()
-o.x = 100
-print(Some.service(o, 200))    # do service... 25
+# 從串列 (List) 來建立集合
+set3 = set([i for i in range(20, 30)])
+
+# 從數組 (Tuple) 來建立集合
+set4 = set((10, 20, 30, 40, 50))
+
+# 集合不會包含重複的資料, 你可以從 set5 印出來的結果得知
+set5 = set((1, 1, 1, 2, 2, 3))
+
+print(set1)
+print(set2)
+print(set3)
+print(set4)
+print(set5)
